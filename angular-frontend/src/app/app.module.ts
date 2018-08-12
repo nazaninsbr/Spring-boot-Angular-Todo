dd95eb93a5f5af2ee3419edc3de0e049450dd3d3
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-08-12 15:58:44 +0430

    all the code

---
+++ angular-frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { AppComponent } from './app.component';
+import { TodoService } from './todo.service';
+
+import { TodoListComponent } from './todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
